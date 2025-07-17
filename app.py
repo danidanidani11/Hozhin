8e8d9d8eac544b82723ae15e2c194f40cfbd0098
danidanidani11 <dssnuandddani@gmail.com>
2025-07-17 23:05:48 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from telegram import Update
-from telegram.ext import Dispatcher, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 from main import start, button_callback, handle_message, admin_reply
 import os
 from dotenv import load_dotenv
@@ -10,27 +10,25 @@ load_dotenv()
 app = Flask(__name__)
 TOKEN = os.getenv("BOT_TOKEN", "7954708829:AAFg7Mwj5-iGwIsUmfDRr6ZRJZr2jZ28jz0")
 
-# تنظیم Dispatcher
-from telegram.ext import Application
-
+# تنظیم Application
 application = Application.builder().token(TOKEN).build()
 dp = application.dispatcher
 
 dp.add_handler(CommandHandler("start", start))
 dp.add_handler(CallbackQueryHandler(button_callback))
-dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
-dp.add_handler(MessageHandler(Filters.photo | Filters.document, handle_message))
-dp.add_handler(MessageHandler(Filters.text & ~Filters.command, admin_reply))
+dp.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+dp.add_handler(MessageHandler(filters.PHOTO | filters.Document.ALL, handle_message))
+dp.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, admin_reply))
 
 @app.route('/')
 def index():
     return "Bot is running!"
 
 @app.route('/webhook', methods=['POST'])
-def webhook():
+async def webhook():
     update = Update.de_json(request.get_json(force=True), application.bot)
-    dp.process_update(update)
+    await dp.process_update(update)
     return 'OK'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
