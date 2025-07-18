4c246e56364d60b9d6ca8a66641f1478deca1dc9
danidanidani11 <dssnuandddani@gmail.com>
2025-07-18 18:20:04 +0330

    Delete set_webhook.py

--- set_webhook.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-TOKEN = "7954708829:AAFg7Mwj5-iGwIsUmfDRr6ZRJZr2jZ28jz0"
-URL = f"https://hozhin.onrender.com/{TOKEN}"  # آدرس سایتت در Render
-
-set_webhook_url = f"https://api.telegram.org/bot{TOKEN}/setWebhook"
-response = requests.get(set_webhook_url, params={"url": URL})
-print(response.json())
